0358d4066c2d5750ca33f394672ca220a28e0af9
bluishhao <734499798@qq.com>
2023-03-27 04:10:20 +0800

    测试二通过

--- app/src/main/java/com/example/cnedujnux2020101351/BowlingGame.java
+++ app/src/main/java/com/example/cnedujnux2020101351/BowlingGame.java
@@ -1,11 +1,12 @@
 package com.example.cnedujnux2020101351;
 
 public class BowlingGame {
+    private int score=0;
     public int score() {
-        return 0;
+        return score;
     }
 
     public void roll(int pin) {
-
+        score+=pin;
     }
 }
