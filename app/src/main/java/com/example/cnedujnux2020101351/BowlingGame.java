13200e6786e9d611972643a2f73f99edec7bbbbd
bluishhao <734499798@qq.com>
2023-03-26 23:29:14 +0800

    测试三通过

--- app/src/main/java/com/example/cnedujnux2020101351/BowlingGame.java
+++ app/src/main/java/com/example/cnedujnux2020101351/BowlingGame.java
@@ -1,12 +1,32 @@
 package com.example.cnedujnux2020101351;
 
 public class BowlingGame {
-    private int score=0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
+    }
+
     public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isSpare(frameIndex))
+            {
+                score += 10 + rolls[frameIndex + 2];
+                frameIndex += 2;
+            } else {
+                score += rolls[frameIndex] +
+                        rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+        }
         return score;
     }
 
-    public void roll(int pin) {
-        score+=pin;
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] +
+                rolls[frameIndex + 1] == 10;
     }
 }
